3656d02f28016a2e50803bb85fe98e8a11a86891
ashunair <ashwathynair1998@gmail.com>
2024-07-06 02:11:22 +0000

    changed backend services ip adress with new one

--- frontend/script.js
+++ frontend/script.js
@@ -9,7 +9,7 @@ document.getElementById('dataForm').addEventListener('submit', function(event) {
         return;
     }
 
-    fetch('http://34.71.199.89:5000/submit', {
+    fetch('http://35.188.107.166:5000/submit', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
